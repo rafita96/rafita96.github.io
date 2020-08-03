c7b6d24d4492032fe6a43324309c21a3f4ac0e6e
Rafael <ra@g.com>
2020-08-02 18:37:06 -0700

    markdown, json o html?

--- blog/src/js/Article.js
+++ blog/src/js/Article.js
@@ -2,12 +2,28 @@ class Article extends React.Component{
 
 	constructor(props){
 		super(props);
+
+		this.state = {
+			texto: "Sabritas"
+		};
+	}
+
+	componentDidMount(){
+		let that = this;
+		fetch(fullpath+"/src/articles/template.md")
+							.then(function(response) {
+								// console.log(response);
+							    return response.text();
+							  })
+							.then(function(md){
+								that.setState({texto: md});
+							});
 	}
 
 	render(){
 		return e('section', {'className': 'article'}, [
 				e('h1', {'key': 'title'}, "Titulo"),
-				e(Content, {'key': 'contenido'}, null)
+				this.state.texto
 			]);
 	}
 }
